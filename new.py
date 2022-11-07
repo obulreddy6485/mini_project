7282fe9614b67ad6690f1f2baac38c522185475e
obulr <obulreddy6485@gmail.com>
2022-11-07 15:25:03 +0530

    f

--- new.py
+++ new.py
@@ -1,1 +1,15 @@
+from gingerit.gingerit import GingerIt
+import streamlit as st
+
+st.title('Grammar & Spell Checker In Python')
+text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+parser = GingerIt()
+if st.button('Correct Sentence'):
+    if text == '':
+        st.write('Please enter text for checking')
+    else:
+        result_dict = parser.parse(text)
+        st.markdown('**Corrected Sentence - **' + str(result_dict["result"]))
+else:
+    pass
 print("Hello world")
